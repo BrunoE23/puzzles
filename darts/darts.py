b7d512f55e2a330da83111ecfc9d1d8364e9f675
BrunoE23 <bruam23@gmail.com>
2023-01-20 17:10:13 +0000

    Complete darts.py

--- darts/darts.py
+++ darts/darts.py
@@ -1,2 +1,18 @@
+import math 
+
 def score(x, y):
-    pass
+    r = math.sqrt(x*x + y*y)
+
+    if r > 10:
+        score = 0
+    elif r > 5:
+        score = 1
+    elif r > 1:
+        score = 5
+    else:
+        score = 10
+
+    return score
+
+
+print(score(-1,1))
